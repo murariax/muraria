1d2664e35dcc74f79f15bff86f8ddad3e599178a
Max <murariax@gmail.com>
2025-09-15 14:57:30 +0300

    commit for rendering to render for the first time

--- muraria/settings.py
+++ muraria/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)5uhanrbpz&fo77z91$(2qwo2o7mlo7zi+u-c5tgu1x-7hsc)7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = []
 
